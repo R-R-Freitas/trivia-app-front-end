0159996c6939d7da8b61ac7356063cf1d7c23d17
Kevin Oliveira <kevin.zero@hotmail.com>
2021-08-04 17:18:17 -0300

    Requisito 02: faz requisicao do token ao entrar no jogo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Feedback from './pages/Feedback';
 import Ranking from './pages/Ranking';
 import Config from './pages/Config';
 import NotFound from './pages/NotFound';
+import Game from './pages/Game';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/feedback" component={ Feedback } />
         <Route path="/ranking" component={ Ranking } />
         <Route path="/config" component={ Config } />
+        <Route path="/game" component={ Game } />
         <Route path="*" component={ NotFound } />
       </Switch>
     </div>
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,22 @@
 export const UPDATE_NAME = 'UPDATE_NAME';
+export const UPDATE_TOKEN = 'UPDATE_TOKEN';
+const TRIVIA_API = 'https://opentdb.com/api_token.php?command=request';
 
 export const updateName = (payload) => ({
   type: UPDATE_NAME,
   payload,
 });
+
+const updateToken = (payload) => ({
+  type: UPDATE_TOKEN,
+  payload,
+});
+
+export const fetchToken = () => (dispatch) => (
+  fetch(TRIVIA_API)
+    .then((data) => data.json())
+    .then((response) => {
+      localStorage.setItem('token', response.token);
+      dispatch(updateToken(response.token));
+    })
+);

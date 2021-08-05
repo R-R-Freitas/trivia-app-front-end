0bb267046b0488c57a660648ba0562f44f4b0219
Kevin Oliveira <kevin.zero@hotmail.com>
2021-08-05 14:33:23 -0300

    Requisito 05: adiciona aleatoriedade nas respostas

--- src/pages/components/Question.js
+++ src/pages/components/Question.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Question extends Component {
+  randomize(array) {
+    for (let lastIndex = array.length - 1; lastIndex > 0; lastIndex -= 1) {
+      const randomIndex = Math.floor(Math.random() * lastIndex);
+      const temp = array[lastIndex];
+      array[lastIndex] = array[randomIndex];
+      array[randomIndex] = temp;
+    }
+    return (array);
+  }
+
   render() {
     const { questionAPI } = this.props;
     const {
@@ -10,21 +20,31 @@ class Question extends Component {
       correct_answer: correctAnswer,
       incorrect_answers: incorrectAnswers,
     } = questionAPI;
+    const correct = (
+      <button
+        type="button"
+        data-testid="correct-answer"
+        key="4"
+      >
+        {correctAnswer}
+      </button>);
+    const incorrect = incorrectAnswers.map((answer, index) => (
+      <button
+        type="button"
+        key={ index }
+        data-testid={ `wrong-answer-${index}` }
+      >
+        {answer}
+      </button>
+    ));
+    const allAnswers = [correct, ...incorrect];
+    console.log(allAnswers);
 
     return (
       <section>
         <h3 data-testid="question-category">{category}</h3>
         <p data-testid="question-text">{question}</p>
-        <button type="button" data-testid="correct-answer">{correctAnswer}</button>
-        {incorrectAnswers.map((answer, index) => (
-          <button
-            type="button"
-            key={ index }
-            data-testid={ `wrong-answer-${index}` }
-          >
-            {answer}
-          </button>
-        )) }
+        { this.randomize(allAnswers) }
       </section>
     );
   }

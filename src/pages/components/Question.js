a9ed33f099466e194c9159008d139aa3e0d78cdb
Eric Faria <ericdeofaria@gmail.com>
2021-08-05 14:48:29 -0300

    Requisito 07: adiciona cores as alternativas

--- src/pages/components/Question.js
+++ src/pages/components/Question.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './question.css';
 
 class Question extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+    }
+
+    this.onClickQuestion = this.onClickQuestion.bind(this);
+  } 
+  
+  onClickQuestion() {
+      this.setState({
+        clicked: true,
+      })
+    };
+
   render() {
     const { questionAPI } = this.props;
     const {
@@ -11,16 +27,28 @@ class Question extends Component {
       incorrect_answers: incorrectAnswers,
     } = questionAPI;
 
+   
+
+    const { clicked } = this.state;
     return (
       <section>
         <h3 data-testid="question-category">{category}</h3>
         <p data-testid="question-text">{question}</p>
-        <button type="button" data-testid="correct-answer">{correctAnswer}</button>
+        <button
+          type="button"
+          data-testid="correct-answer"
+          onClick={ this.onClickQuestion }
+          className={ clicked && 'correctAnswer' }
+          >
+            {correctAnswer}
+          </button>
         {incorrectAnswers.map((answer, index) => (
           <button
             type="button"
             key={ index }
             data-testid={ `wrong-answer-${index}` }
+            onClick={ this.onClickQuestion }
+            className={ clicked && 'incorrectAnswer' }
           >
             {answer}
           </button>

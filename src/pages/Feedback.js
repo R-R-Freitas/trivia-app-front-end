b3b8388c75f9b04dbcdfc2407a6d84371137f8ba
Eric Faria <ericdeofaria@gmail.com>
2021-08-06 15:17:49 -0300

    Requisito 16 - Renderiza botão de ranking na tela de feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -27,6 +27,11 @@ class Feedback extends Component {
             Jogar novamente
           </button>
         </Link>
+        <Link to="/ranking">
+          <button type="button" data-testid="btn-ranking">
+            Ver Ranking
+          </button>
+        </Link>
       </div>
     );
   }

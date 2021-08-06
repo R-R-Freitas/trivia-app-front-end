7869ade525c15afd1952dc428914c924aaed7f7f
Eric Faria <ericdeofaria@gmail.com>
2021-08-06 14:59:58 -0300

    Requisito 13 - Renderiza mensagem de feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { assertions } = this.props;
+    const minAssertions = 3;
     return (
       <div>
         <Header />
+        <p data-testid="feedback-text">
+          { assertions >= minAssertions ? 'Mandou bem!' : 'Podia ser melhor...!' }
+        </p>
       </div>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
